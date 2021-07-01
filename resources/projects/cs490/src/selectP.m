f256c2330005bb1779fb5af8e772f20823055a13
Justin Tung <justin.tung@live.ca>
2021-07-01 14:27:59 -0400

    extract to separate repository

--- resources/projects/cs490/src/selectP.m
+++
@@ -1,8 +0,0 @@
-function [alpha, beta, xt, xm] = selectP()
-% SELECTP  Used to set fp model parameters from user input
-
-disp('Please select fixed-point values')
-alpha = input('alpha: ')
-beta = input('beta: ')
-xt = input('xt: ')
-xm = input('xm: ')
